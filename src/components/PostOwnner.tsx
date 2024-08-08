60d808312780524a23f844d550dc1faab6fab429
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-08-08 14:49:11 +0700

    put mypic

--- src/components/PostOwnner.tsx
+++ src/components/PostOwnner.tsx
@@ -7,7 +7,7 @@ export default function PostOwnner(props : any) {
     <div className="vstack gap-3">
       <div className="d-flex align-items-center gap-3">
         <img
-          src="/profileImages/handsome.jpg"
+          src="/profileImages/mypic.png"
           width="48"
           height="48"
           className="rounded-circle"
